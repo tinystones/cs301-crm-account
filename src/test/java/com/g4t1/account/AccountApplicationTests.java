0f3ef8aff2a6cee0a1e3549eb29e6987a4655682
JChua <145314285+tinystones@users.noreply.github.com>
2025-09-14 12:20:46 +0800

    initial commit

---
+++ src/test/java/com/g4t1/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g4t1.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
